e6d7c8e09b1a920f6fceba517f2f00badd47c927
Sam Nelson <12samn@gmail.com>
2021-02-21 13:43:34 -0800

    methods to shuffle data files

--- kh2lib/memoryparser.py
+++ kh2lib/memoryparser.py
@@ -91,3 +91,110 @@ class memoryparser:
                 bi += 1
             replace = not replace
         open(fn, "wb").write(f)
+    def readable_data(self, fn, record_spec, header_length=0x0, footer_length=0x0,hide_indices=[],swapendian=False, showinhex=True, aspandas=False, displayHeader=False,displayFooter=False, recordstoshow=None):
+        def _readable(ba):
+            return " ".join([hex(i)[2:].zfill(2).upper() for i in ba])
+        if aspandas:
+            import pandas as pd
+
+        f = bytearray(open(fn, "rb").read())
+
+        header = f[:header_length]
+
+        records = f[header_length:len(f)-footer_length]
+        rec_len = sum(record_spec)
+        num_records = len(records) // sum(record_spec)
+
+        footer = f[len(f)-footer_length:]
+
+        if displayHeader:
+            print("HEADER")
+            print(_readable(header))
+            print("\n")
+        if displayFooter:
+            print("FOOTER")
+            print(_readable(footer))
+            print("\n")
+        print("\n\n\n")
+
+        df = ''
+        recs = []
+        for x in range(num_records):
+            if recordstoshow and x > recordstoshow:
+                continue
+            r = []
+            prev = 0
+            for i in record_spec:
+                r.append([records[rec_len*x+im+prev] for im in range(i)])
+                prev += i
+            if swapendian:
+                for v in range(len(r)):
+                    var = r[v]
+                    if len(var) > 1:
+                        r[v] = var[::-1]
+            # Convert to more readable form
+            for v in range(len(r)):
+                var = r[v]
+                var = _readable(var).replace(" ", "")
+                if not showinhex:
+                    var = int(var, 16)
+                if v in hide_indices:
+                    var = "__"
+                r[v] = var
+            recs.append(r)
+
+        if not aspandas:
+            for rec in recs:
+                if not showinhex:
+                    print(rec)
+                else:
+                    print(" ".join(rec))
+        else:
+            df = pd.DataFrame(recs)
+        return df
+
+
+    def shuffle_data(self, fn, record_spec=None, header_length=0x0, footer_length=0x0, indices_to_shuffle=None):
+        f = bytearray(open(fn, "rb").read())
+        import random
+        header = f[:header_length]
+
+        records = f[header_length:len(f)-footer_length]
+        rec_len = sum(record_spec)
+        num_records = len(records) // sum(record_spec)
+
+        footer = f[len(f)-footer_length:]
+        recs = []
+        for x in range(num_records):
+            r = []
+            prev = 0
+            for i in record_spec:
+                r.append([records[rec_len*x+im+prev] for im in range(i)])
+                prev += i
+            recs.append(r)
+            
+        # Default shuffle everything
+        if not indices_to_shuffle:
+            indices_to_shuffle = list(range(len(recs[0])))
+            
+        lists_of_indices = [[n[i] for n in recs] for i in range(len(recs[0]))]
+        for i in range(len(lists_of_indices)):
+            if i in indices_to_shuffle:
+                random.shuffle(lists_of_indices[i])
+
+        recs = [[] for r in recs]
+        for indi in lists_of_indices:
+            for r in range(len(indi)):
+                for bt in indi[r]:
+                    recs[r].append(bt)
+        recs_new = []
+        for rec in recs:
+            for i in rec:
+                recs_new.append(i)
+                
+
+        new_file = header + bytearray(recs_new) + footer
+
+        open(fn, "wb").write(new_file)        
+
+
